02bceaa4999a3383dfca085909aab5f5e7682e67
Kristen Groller <groller@rogers.com>
2021-04-24 17:15:10 -0400

    everything but portfolio working

---
+++ src/components/Project/index.js
@@ -0,0 +1,37 @@
+// import React, { useState } from 'react';
+// import { capitalizeFirstLetter } from '../../utils/helpers';
+
+// function Project  ({ currentCategory })  {
+//         // const [isModalOpen, setIsModalOpen] = useState(false);
+//         // const [currentPortfolio, setCurrentPortfolio] = useState();
+//         const [projects] = useState([
+//             {
+//                 name: 'e-z-Budget',
+//                 description: 'Full stack group project',
+//                 category: 'e-z-Budget'
+//             },
+//             {
+//                 name: 'reFrain',
+//                 description: 'Group project'
+//             },
+//             {
+//                 name: 'e-z-Budget',
+//                 description: 'Full stack group project'
+//             },
+//             {
+//                 name: 'e-z-Budget',
+//                 description: 'Full stack group project'
+//             },
+//         ]);
+
+
+//     return (
+//         <section>
+//           <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+//           <p>{currentCategory.description}</p>
+//           <Project category={currentCategory.name} />
+//         </section>
+//       );
+//     }
+            
+// export default Project;
