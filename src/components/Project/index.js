bd3d8e6e2447f0181bc35c1f532f07804d3390fd
Kristen Groller <groller@rogers.com>
2021-05-23 11:11:12 -0400

    yet again

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -13,35 +13,35 @@ const Project = () => {
                 </a>
             </div>
             <div>
-                <a href="//ez-budget10.herokuapp.com/" class="grid-item e-z-Budget">
+                <a href="/ez-budget10.herokuapp.com/" class="grid-item e-z-Budget">
                     <h3>E-Z-BUDGET</h3>
                     <span>Group Project 2/Fullstack Program</span>
                 </a>
             </div>
 
             <div>
-                <a href="https:codinggroupproject1.github.io/REFRAIN/" class="grid-item REFRAIN">
+                <a href="/REFRAIN/" class="grid-item REFRAIN">
                     <h3>REFRAIN</h3>
                     <span>HTML/CSS</span>
                 </a>
             </div>
 
             <div>
-                <a href="https:kgroll.github.io/run-buddy/" class="grid-item run-buddy">
+                <a href="/run-buddy/" class="grid-item run-buddy">
                     <h3>RUN BUDDY</h3>
                     <span>HTML/CSS</span>
                 </a>
             </div>
 
             <div>
-                <a href="https:kgroll.github.io/urban-octo-telegram/" class="grid-item Horiseon">
+                <a href="/urban-octo-telegram/" class="grid-item Horiseon">
                     <h3>HORISEON MARKETING</h3>
                     <span>HTML/CSS</span>
                 </a>
             </div>
 
             <div>
-                <a href="https:kgroll.github.io/weather-dashboard/" class="grid-item weather-dashboard">
+                <a href="/weather-dashboard/" class="grid-item weather-dashboard">
                     <h3>WEATHER DASHBOARD</h3>
                     <span>JavasScript/
             HTML/CSS</span>
